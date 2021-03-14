15d6293df79acfd847ddd6f4032424fcbfa38156
hotlynn2 <laizer421@gmail.com>
2021-03-14 23:10:31 +0000

    commit

--- app.py
+++ app.py
@@ -30,12 +30,40 @@ def main():
         X_train, y_train, X_test, y_test = train_test_split(X, y, test_size = 0.25, random_state = 120)
         return X_train, y_train, X_test, y_test
 
+    def plot_metrics(metrics_list):
+        if 'Confusion Matrix' in metrics_list:
+            st.subheader('Confusion Matrix')
+            plot_confusion_matrix(model, X_test, y_test, displat_labels = class_names)
+            st.pyplot()
+
+        if 'ROC Curve' in metrics_list:
+            st.subheader('ROC Curve')
+            plot_confusion_matrix(model, X_test, y_test)
+            st.pyplot()
+
+        if 'Precision-Recall Curve' in metrics_list:
+            st.subheader('Precision-Recall Curve')
+            plot_confusion_matrix(model, X_test, y_test)
+            st.pyplot()
+
     df = load_df()
     X_train, y_train, X_test, y_test = split(df) 
+    class_names = ['edible', 'poisonous']
+    
+
+
+
+
+
+
     
     if st.sidebar.checkbox('show raw dataset', False):
         st.subheader('Mushroom Data Set (Classification)')
         st.write(df)
+
+    # if st.sidebar.select_slider('Confusion Matrix', False):
+    #     st.subheader('Confusion Matrix Plot')
+    #     st.write(metrics)
 
 
 
