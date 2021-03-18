974dec5045ef13732e4fc5a25b8ce471df6d77eb
hotlynn2 <laizer421@gmail.com>
2021-03-18 22:07:59 +0000

    commit

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ def main():
 
     if classifier == 'Support Vector Machine (SVM)':
         st.sidebar.subheader('Model Hyperparameters')
-        C = st.sidebar.number_input("C (Regularization Number", 0.01, 10.0, step = 0.01, key = C)
-        kernel = st.sidebar.radio("Kernel", ("rbf","linear"), key = kernel)
-        gamma = st.sidebar.radio("Gamma Kernel Coefficient", ("scale", "auto"), key = gamma )
+        C = st.sidebar.number_input("C (Regularization Number)", 0.01, 10.0, step = 0.01, key = 'C')
+        kernel = st.sidebar.radio("Kernel", ("rbf","linear"), key = 'kernel')
+        gamma = st.sidebar.radio("Gamma Kernel Coefficient", ("scale", "auto"), key = 'gamma' )
 
 
 
